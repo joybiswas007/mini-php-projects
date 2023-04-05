7bd8b0faf10d7f5d9630686667f482b7d8ad6fee
Joy Biswas <joybiswas040701@gmail.com>
2023-04-05 15:05:46 +0600

    added files via upload

--- Password_Generator.php
+++ Password_Generator.php
@@ -5,4 +5,10 @@
     A simple Password Generator for generating strong passwords
  *********************************************************************************
 **********************************************************************************/
+function password_generator($length)
+{
+   $data = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789~!@#$%^&*()-_=+/\|<>,.?:;";
+   return substr(str_shuffle($data), 0, $length);
+}
 
+echo "Your Password is: " . password_generator(40) . PHP_EOL;
